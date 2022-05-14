d488010b80c53ec23709ce0032c8c154cf7386a1
KihunSung <hikihun3@gmail.com>
2022-05-14 18:08:06 +0900

    first commit
    add Login page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
+import logo from './Logo_Login.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Login></Login>
     </div>
   );
 }
 
 export default App;
 
-
 function Header() {
   return (
     <div>
       <h1>Header</h1>
     </div>
+  )
+}
+
+function Login() {
+  return(
+    <img src={logo} ></img>
   )
 }
