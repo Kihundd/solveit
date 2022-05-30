d7fce9152884872bbbdf0be68df07aa2e74c6530
KihunSung <hikihun3@yu.ac.kr>
2022-05-31 01:39:41 +0900

    feat: add Liked

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -3,11 +3,23 @@ import QuestionNum from '../components/takeTest/QuestionNum';
 import QuestionView from "../components/takeTest/QuestionView";
 import TestName from "../components/takeTest/TestName";
 import { Container, Grid } from '@mui/material'
-
-
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { TAKE_TEST } from "../queries/queries";
+import { useParams } from "react-router-dom";
 
 
 function Test(){
+
+    const [questionids, setQuestionids] = useState([])
+    const params = useParams();
+    const {data, loading, error} = useQuery(TAKE_TEST, {
+        variables:{id: params.testId}
+    });
+    console.log(data)
+
+    if(loading) return <p>Loading...</p>;
+    if(error) return <p>Error!</p>;
 
     return(
         <>
