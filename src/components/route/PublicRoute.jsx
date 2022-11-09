84b599c1a8f646bc680b7e541c0165839c000374
Kihundd <hikihun3@yu.ac.kr>
2022-11-09 14:18:01 +0900

    Revert "commit"
    This reverts commit a845333e90fcd96b5e49b4d580ba7165c4cf175b.

--- src/components/route/PublicRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import isLogin from '../../pages/isLogin';
-
-const PublicRoute = ({ children }) => {
-    return !isLogin ? <Navigate to='/' /> : children;
-}
-export default PublicRoute
